01a3c2fd89310b0c8b1c1ca55746487401f72de8
Neyda Morales <neydaa.morales@gmail.com>
2024-06-17 18:02:17 -0400

    hw 6

---
+++ Starter_Code/starter_code/api_keys_booth.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "021931b9b9945cadc36ca9a567f3427a"
+
+# Geoapify API Key
+geoapify_key = "2d995fa25b1f40fab4230313afdf401a"
---
+++ Submission/api_keys_neyda.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "568a4f8682d886a5a6360eb6b958456e"
+
+# Geoapify API Key
+geoapify_key = "36871df74c894b34885c04f91d36e65c"
